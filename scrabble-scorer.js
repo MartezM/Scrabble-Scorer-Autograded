806f8108c3e91cc2f0604d7fa3860509e1baa816
Martez <mmorgan5448@gmail.com>
2023-10-14 18:48:50 -0500

    working on it

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -9,7 +9,7 @@ const oldPointStructure = {
   4: ['F', 'H', 'V', 'W', 'Y'],
   5: ['K'],
   8: ['J', 'X'],
-  10: ['Q', 'Z']
+  10: ['Q', 'Z'] 
 };
 
 function oldScrabbleScorer(word) {
@@ -23,7 +23,7 @@ function oldScrabbleScorer(word) {
 		 if (oldPointStructure[pointValue].includes(word[i])) {
 			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
 		 }
- 
+  
 	  }
 	}
 	return letterPoints;
@@ -36,33 +36,88 @@ function initialPrompt() {
  word = input.question(`Let's play some scrabble! 
  Enter a word for points: `);
 console.log( `${word}
-${oldScrabbleScorer(word)}`);
+${scorerPrompt(word)}`);
    return word;
 };
 
 let simpleScorer = function(word){
    word = word.toUpperCase();
  let score = word.length ;
- console.log(`your score is: ${score}`);
+ for (i = 0; i < word.length; i++){
+   console.log(word[i], '+ 1');
+ }
+ return console.log('your word value is:', score)
 }
 
 let vowelBonusScorer = function(word){
-   let vowels = ['a','E']
-
+   word = word.toUpperCase();
+   let vowel = ['A','E','I','O','U'];
+   let vScore = 0;
+   
+   for (i = 0; i < word.length; i++){
+      let newstr = word[i];
+      if (vowel.includes(newstr)){
+        vScore = vScore + 3
+         console.log(word[i], "+ 3")
+      } else {
+        vScore = vScore + 1
+        console.log(word[i], "+ 1")
+      }
+      
+   }
+   return console.log('Your word value is :', vScore)
 }
 
 let scrabbleScorer;
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [
+   {name: 'Scramble', Discription: "Each letter is worth 1 point.", ScoreFunction: simpleScorer},
+   {name: 'BonusVowels', Discription: "Vowels are 3pts, constants are 1 pt." , ScoreFunction: vowelBonusScorer},
+   {name: 'SimpleScore', Discription: "The traditional scoring algorithm." , ScoreFunction: transform}
+];
 
-function scorerPrompt() {}
+function scorerPrompt() {
+   let answer = input.question(`Which scoring algorithm would you like to use today?
+   0 - ${scoringAlgorithms[0].name}: ${scoringAlgorithms[0].Discription}
+   1 - ${scoringAlgorithms[1].name}: ${scoringAlgorithms[1].Discription}
+   2 - ${scoringAlgorithms[2].name}: ${scoringAlgorithms[2].Discription}
+   Enter 0, 1, or 2 to get your score! :`);
+   answer = Number(answer);
+   if (!isNaN(answer) && answer < 3){
+       console.log(answer)
+       return scoringAlgorithms[answer].ScoreFunction(word)
+     
+   }
+}
 
-function transform() {};
+function transform(word) {
+  word = word.toUpperCase;
+   let scr;
+   let nstr = '';
+   for (i = 0; i > 7; i++);
+   nstr = word[i];
+   if(newPointStructure[i].name.includes(nstr)){
+      scr = newPointStructure[i].point + scr
+   }
+   console.log(scr);
+   console.log(nstr)
+}
 
-let newPointStructure;
+let newPointStructure = [
+   {name: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'], point: 1},
+   {name: ['D', 'G'], point: 2},
+   {name: ['B', 'C', 'M', 'P'], point: 3},
+   {name: ['F', 'H', 'V', 'W', 'Y'], point: 4},
+   {name: ['K'], point: 5},
+   {name: ['J', 'X'], point: 8},
+   {name: ['Q', 'Z'], point: 10}
+   
+];
+
 
 function runProgram() {
    initialPrompt();
+   scorerPrompt();
    
 }
 
